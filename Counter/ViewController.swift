15a4b8dc1e43fe08f3d544a89a14b17d5c76d208
Anna Nguyen <anguyen@Roberts-Mac-mini.local>
2017-09-22 19:09:25 -0500

    Add project

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Anna Nguyen on 9/21/17.
-//  Copyright © 2017 Anna Nguyen. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var numCount: UILabel!
+    
+    @IBOutlet weak var labelConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var screen: UIView!
+    
+    var count = 0
+    
+    //These variables prevent the label from going off of the top/bottom of the phone initially set for portrait mode
+    var upBound = 250
+    var downBound = -250
+    
+    
+    //When up button is pressed causes the label numCount to move up and increment
+    @IBAction func upButtonTapped(_ sender: Any) {
+        moveAndUpdateCounter(increment: 5)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //When down button is pressed causes the label numCount to move down and decrement
+    @IBAction func downButtonTapped(_ sender: Any) {
+        moveAndUpdateCounter(increment: -5)
     }
-
-
+    
+    //Increments/Decrements the counter as long as it's in the bounds which change depending on whether the device is in portrait or landscape mode
+    //Paramenter increment is an Int, set in the upButtonTapped/downButtonTapped functions
+    func moveAndUpdateCounter(increment: Int){
+        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            upBound = 145
+            downBound = -145
+        }
+        if count < upBound && increment > 0 {
+            count+=increment
+            numCount.text="\(count)"
+            labelConstraint.constant -= CGFloat(increment)
+            view.setNeedsLayout()
+        }
+        else if increment < 0 && count > downBound {
+            count+=increment
+            numCount.text="\(count)"
+            labelConstraint.constant -= CGFloat(increment)
+            view.setNeedsLayout()
+        }
+    }
 }
-
